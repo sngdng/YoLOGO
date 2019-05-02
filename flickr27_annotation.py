67fbbd54369756b8b878ce21d185b031aac9d98e
lafius <duong.spthien@hotmail.fr>
2019-05-02 12:00:09 +0200

    annotations fixed

--- flickr27_annotation.py
+++ flickr27_annotation.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 #brand = ["Adidas", "Apple", "BMW", "Citroen", "Coca Cola", "DHL", "Fedex", "Ferrari", "Ford", "Google", "Heineken", "HP", "McDonalds", "Mini", "Nbc", "Nike", "Pepsi", "Porsche", "Puma", "Red Bull", "Sprite", "Starbucks", "Intel", "Texaco", "Unisef", "Vodafone", "Yahoo"]
 
@@ -8,7 +9,7 @@ SOURCE_DIR = os.getcwd()
 
 train_data = SOURCE_DIR + "/data/train_annotation.txt"
 
-new_annotation = []
+new_annotation = defaultdict(list)
 
 with open(train_data, "r") as data:
     lines = data.readlines()
@@ -17,9 +18,10 @@ with open(train_data, "r") as data:
         path = SOURCE_DIR + "/data/images/" + strLine[0]
         if strLine[1] in brand:
             class_id = brand.index(strLine[1])
-            new_annotation.append(path + " " + strLine[-4] + "," + strLine[-3] + "," + strLine[-2] + "," + strLine[-1] + "," + str(class_id))
+            if(not strLine[-4] + "," + strLine[-3] + "," + strLine[-2] + "," + strLine[-1] + "," + str(class_id) in new_annotation[path]):
+                new_annotation[path].append(strLine[-4] + "," + strLine[-3] + "," + strLine[-2] + "," + strLine[-1] + "," + str(class_id))
 
 with open("flickr5_train.txt", "w") as f:
-    for a in new_annotation:
-        f.write(a + "\n")
+    for path in new_annotation:
+        f.write(path + " " +" ".join(new_annotation[path]) + "\n")
 f.close()
